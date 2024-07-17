5cbddbbae555429cac071ea50272b6ae4d115c4d
Idsfjkl2 <memepig52@gmail.com>
2024-07-17 12:12:02 -0400

    Update client.js

--- client.js
+++ client.js
@@ -19367,6 +19367,7 @@ Animal.prototype.basicDrawSkinImg = function() {
                this.specType22 = 0
 	      }
 						if (this.specType2 >= 0) {
+							console.log(this.specType2 - this.specType22)/(50 - this.specType * 49/255))
                                          this.specType22 += (this.specType2 - this.specType22)/(50 - this.specType * 49/255)
 						}
 			if (this.specType22 > 0) {
