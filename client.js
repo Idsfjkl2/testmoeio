c01b175300fcf87797a8fd2ecdaf5ca153ad5df1
Idsfjkl2 <memepig52@gmail.com>
2024-07-18 07:29:23 -0400

    Update client.js

--- client.js
+++ client.js
@@ -14758,8 +14758,8 @@ case ability_thunderbirdAttack:
           var extraRotate = -(-0.5 + rotfac0to1) * toRadians(135.0); //spin animation
 
           //clip to sliwly show the claw
-          var rad = this.rad * 0.6;
-          ctx.rotate(this.angle + toRadians(45.0) + extraRotate);
+          var rad = this.rad;
+          ctx.rotate(this.angle + toRadians(90.0) + extraRotate);
           var imX = 0,
             imY = this.rad;
           var imW = rad * 2.0 * 0.7,
