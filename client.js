1027d981698105c3b3cd4c6063fef3ac67a3bd7d
Idsfjkl2 <memepig52@gmail.com>
2024-07-23 14:30:29 -0400

    Update client.js

--- client.js
+++ client.js
@@ -20448,7 +20448,7 @@ Animal.prototype.getSkinName = function() {
 			  if (!this.telespawntime) {
                         this.telespawntime = timestamp
 			  }
-			  let img = Math.trunc(this.telespawntime / 120)
+			  let img = Math.trunc((this.telespawntime - timestamp) / 120)
                       skinName = skinFolder + skinName + "tele" + img;
 		  } else {
 			  this.telespawnTime = false
