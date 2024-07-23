3d6759539d92c2274b543ee6a8d321875fd84ce8
Idsfjkl2 <memepig52@gmail.com>
2024-07-23 15:58:42 -0400

    Update client.js

--- client.js
+++ client.js
@@ -20798,9 +20798,6 @@ for (var J = 0; J < cnt; J++) {
     this.flag_inColdOcean = aniFlags.includes(41)
       this.flag_invisible = aniFlags.includes(42)
   this.flag_oldinvisible = aniFlags.includes(42)
-		if (this.telespawntime && Math.trunc((timestamp - this.telespawntime) / 40) > 3) {
-              this.flag_oldinvisible = true
-		}
         this.flag_SeeID = aniFlags.includes(43)
          this.flag_ingenjutsu = aniFlags.includes(44)
            this.flag_indeepsea = aniFlags.includes(45)
