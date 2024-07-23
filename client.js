89a5518663b63a34a95cc80d93af727bb559cb09
Idsfjkl2 <memepig52@gmail.com>
2024-07-23 19:12:39 -0400

    Update client.js

--- client.js
+++ client.js
@@ -19494,9 +19494,9 @@ if (this.animalType == a_kj && this.specType2 == 1) ctx.rotate(-this.angle);
 		ctx.rotate(rotation3)
 		              var theImg = getLoadedImg("img/firecrash.png");
 		        if (theImg) {
-				oldrad *= Math.min(4/3, (Date.now() - firetimestamp)/750)
-				if ((Date.now() - firetimestamp) > 1000) {
-					console.log(Math.max(0.75, 1 - ((Date.now() - firetimestamp - 1000)/1000)))
+				oldrad *= Math.min(4/3, (Date.now() - firetimestamp)/)(750/2))
+				if ((Date.now() - firetimestamp) > 500) {
+					console.log(Math.max(0.75, 1 - ((Date.now() - firetimestamp - 500)/500)))
                                  oldrad *= Math.max(0.75, 1 - ((Date.now() - firetimestamp - 1000)/1000))
 				}
 				oldrad *= 4/3
